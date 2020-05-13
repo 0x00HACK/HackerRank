eaf2352e7b8ef1ce65914e633aba56fe30bf1933
Rahul <36473097+0x00HACK@users.noreply.github.com>
2020-05-13 22:01:40 +0530

    Create Day 10: Binary Numbers.cpp

---
+++ 30 Days of Code/CPP/Day 10: Binary Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int remainder,count1=0,count2=0;
+    while(n>0)
+    {
+        remainder=n%2;
+        n/=2;
+        if(remainder==1)
+        {
+            ++count1;
+            if(count1>=count2)  count2=count1;
+        }
+        else    count1=0;
+    }
+    cout<<count2;
+    return 0;
+}
