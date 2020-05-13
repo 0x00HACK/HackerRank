7e6ad38bbc000bd35d31c92d58684f17344f5979
Rahul <36473097+0x00HACK@users.noreply.github.com>
2020-05-13 22:04:20 +0530

    Create Day 10: Binary Numbers.java

---
+++ 30 Days of Code/JAVA/Day 10: Binary Numbers.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int remainder,count1=0,count2=0;
+        while(n>0)
+        {
+            remainder=n%2;
+            n/=2;
+            if(remainder==1)
+            {
+                ++count1;
+                if(count1>=count2)  count2=count1;
+            }
+            else    count1=0;
+        }
+        System.out.print(count2);
+        scanner.close();
+    }
+}
